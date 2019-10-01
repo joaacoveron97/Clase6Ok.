2876783d61e181541ec6438499f786a51f2848f5
Aplicaciones Moviles <Aplicaciones Moviles@NC1363>
2019-09-30 21:29:05 -0300

    Agregar archivos de proyecto.

---
+++ Clase6Ok/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Clase6Ok
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random ObjetoRandom = new Random();
+            int numero = 0;
+
+            for (int i = 0; i < 10; i++) 
+            {
+                numero += ObjetoRandom.Next(1000); 
+            }
+            Console.WriteLine("El promedio de los diez numeros soretados es:  " + (numero / 10)); 
+        }
+    }
+}
+
+        
+    
+
